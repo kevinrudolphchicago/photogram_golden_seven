8a890df719d03a9441355c97aef52a69a305b0c4
kevinrudolphchicago <kevinrudolph571@gmail.com>
2015-08-05 14:55:16 -0700

    Rudolph_photogram_golden_seven

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,55 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find(params["id"])
+
+  end
+
+  def new_form
+
+  end
+
+
+  def create_row
+    p = Photo.new
+    # p = Photo.new # for initializing and creating a new record
+    p.caption = params["photo_caption"]
+    p.source = params["photo_image_url"]
+    p.save
+    redirect_to("http://localhost:3000/photos")
+  end
+
+
+  def destroy
+    p = Photo.find(params["id"])
+    p.destroy
+    p.save
+    redirect_to("http://localhost:3000/photos")
+  end
+
+
+  def update_row
+    # raise params.inspect
+    p = Photo.find(params["id"])
+    # I don't think we want to destroy it, just edit it
+    # p.destroy
+    # p = Photo.find(params["id"])
+    p.caption = params["the_caption"]
+    p.source = params["the_source"]
+    p.save
+
+
+        redirect_to("http://localhost:3000/photos/#{p.id}")
+
+  end
+
+
+
+  def edit_form
+    @photo = Photo.find(params["id"])
+
+  end
 
 end
+
